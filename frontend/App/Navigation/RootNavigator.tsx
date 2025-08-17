48502ea81b37baf6c4f6402aa20afdc2af627641
ismailerguldev <ismailergul.dev@gmail.com>
2025-08-17 19:15:46 +0300

    Auth Screen Updated

--- frontend/App/Navigation/RootNavigator.tsx
+++ frontend/App/Navigation/RootNavigator.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import HomeStack from "./Stacks/HomeStack"
 import AuthScreen from "./Stacks/AuthScreen"
+import { StatusBar } from "expo-status-bar"
 const Stack = createStackNavigator()
 const RootNavigator = () => {
     const [isLogged, setLogged] = useState<boolean>(false)
@@ -29,6 +30,7 @@ const RootNavigator = () => {
                 <Stack.Screen name="HomeStack" component={HomeStack} />
                 <Stack.Screen name="AuthPage" component={AuthScreen} />
             </Stack.Navigator>
+            <StatusBar style='light' />
         </NavigationContainer>
     )
 }
--- frontend/App/Navigation/Stacks/AuthScreen.tsx
+++ frontend/App/Navigation/Stacks/AuthScreen.tsx
@@ -1,15 +1,15 @@
-import { View, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Dimensions, Text, TextInput, KeyboardAvoidingView } from 'react-native';
 import React, { useState } from 'react';
-import Animated, { BounceInLeft, BounceInRight } from 'react-native-reanimated';
-import { Button, Text, TextInput } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from "@react-navigation/stack"
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { RootStackParams } from '../../Sources/Models/models';
 import Entypo from '@expo/vector-icons/Entypo';
-
+import { LinearGradient } from 'expo-linear-gradient';
+import { Button } from 'react-native-paper';
 type AuthScreenNavigationProp = StackNavigationProp<RootStackParams>;
-
+import Animated, { BounceInRight } from "react-native-reanimated"
+import { MotiView } from 'moti';
 const AuthScreen = () => {
     const navigation = useNavigation<AuthScreenNavigationProp>();
     const { width, height } = Dimensions.get("screen");
@@ -19,6 +19,11 @@ const AuthScreen = () => {
     const [password, setPassword] = useState<string>('');
     const [isVisible, setIsVisible] = useState<boolean>(false);
     const [error, setError] = useState<string>('');
+    const [inputKey, setInputKey] = useState<number>(0)
+    const [randomX, setRandomX] = useState<number>(0)
+    const [randomY, setRandomY] = useState<number>(0)
+    const [randomX2, setRandomX2] = useState<number>(0)
+    const [randomY2, setRandomY2] = useState<number>(0)
     const handleAuth = async () => {
         if (isSignUp) {
             if (!username || !email || !password) {
@@ -26,7 +31,6 @@ const AuthScreen = () => {
             } else if (password.length < 8) {
                 setError("The password must be longer than 8 characters.");
             } else {
-                console.log("kayıt");
                 await fetch("http://192.168.1.76:5000/register", {
                     method: "POST",
                     headers: {
@@ -71,51 +75,70 @@ const AuthScreen = () => {
             });
     };
     return (
-        <View style={{ width: width, height: height, justifyContent: "center", alignItems: "center", gap: 15, }}>
-            <View style={{ width: width * 0.7, gap: 10, }}>
+        <KeyboardAvoidingView behavior="padding" style={{ width: width, height: height, justifyContent: "center", alignItems: "center", gap: 15, backgroundColor: "#151515" }}>
+            <LinearGradient colors={["#57379a", "transparent"]} style={{
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                top: 0,
+                height: height * 0.45,
+            }} />
 
-                <Animated.Text entering={BounceInLeft.duration(500)} style={{ fontSize: 24, fontFamily: "Itim" }}>
+            <View style={{ width: width * 0.8, gap: 15, alignItems: "center", zIndex: 1 }}>
+                <Text style={{ color: "white", fontSize: 32, }}>{isSignUp ? "Sign Up" : "Sign In"}</Text>
+                <Animated.View entering={BounceInRight.springify().duration(800)} style={{ width: "100%", gap: 15 }} key={inputKey}>
+                    {
+                        isSignUp &&
+                        <TextInput style={{
+                            borderColor: "#aeaeae", borderWidth: 0.3, borderRadius: 5, color: "white", paddingHorizontal: 20, height: height * 0.05
+                        }} placeholder='Username' placeholderTextColor="white" onChangeText={setUsername} />
+                    }
+                    <TextInput style={{
+                        borderColor: "#aeaeae", borderWidth: 0.3, borderRadius: 5, color: "white", paddingHorizontal: 20, height: height * 0.05
+                    }} placeholder='Email' placeholderTextColor="white" onChangeText={setEmail} />
+                    <TextInput style={{
+                        borderColor: "#aeaeae", borderWidth: 0.3, borderRadius: 5, color: "white", paddingHorizontal: 20, height: height * 0.05
+                    }} placeholder='Password' placeholderTextColor="white" onChangeText={setPassword} />
+                </Animated.View>
+                <Button onPress={handleAuth} mode='outlined' textColor='white' theme={{ colors: { outline: "#6443acff" } }} style={{ paddingHorizontal: 20, paddingVertical: 3, borderRadius: 10 }}>
                     {isSignUp ? "Sign Up" : "Sign In"}
-                </Animated.Text>
-
-                {isSignUp && (
-                    <Animated.View entering={BounceInRight.duration(500).delay(100).springify()} style={{ width: "100%" }}>
-                        <TextInput label="Username" placeholder="Username" mode="outlined" onChangeText={(value) => setUsername(value.trim())} />
-                    </Animated.View>
-                )}
-
-                <Animated.View entering={isSignUp ? BounceInRight.duration(500).delay(200).springify() : BounceInRight.duration(500).delay(100).springify()} style={{ width: "100%" }}>
-                    <TextInput label="Email" inputMode="email" autoCapitalize="none" placeholder="Email" mode="outlined" onChangeText={(value) => setEmail(value.trim())} />
-                </Animated.View>
-
-                <Animated.View entering={isSignUp ? BounceInRight.duration(500).delay(300).springify() : BounceInRight.duration(500).delay(200).springify()} style={{ width: "100%" }}>
-                    <TextInput secureTextEntry={!isVisible} label="Password" placeholder="Password" mode="outlined" onChangeText={(value) => setPassword(value.trim())} />
-                    <TouchableOpacity onPress={() => setIsVisible(!isVisible)} style={{ position: "absolute", right: 0, top: "50%", transform: [{ translateY: -15 }], }}>
-                        {isVisible ? (
-                            <Entypo name="eye-with-line" size={24} color="black" />
-                        ) : (
-                            <Entypo name="eye" size={24} color="black" />
-                        )}
-                    </TouchableOpacity>
-                </Animated.View>
-
-                {error && (
-                    <Text style={{ color: "darkred", fontFamily: "Itim" }}>
-                        {error}
-                    </Text>
-                )}
-                <Animated.View entering={isSignUp ? BounceInRight.duration(500).delay(400) : BounceInRight.duration(500).delay(300)}>
-                    <Button buttonColor="#a7c7c7" mode="contained" onPress={handleAuth}>
-                        <Text>{isSignUp ? "Sign Up" : "Sign In"}</Text>
-                    </Button>
-                </Animated.View>
-                <Animated.View entering={isSignUp ? BounceInRight.duration(500).delay(500) : BounceInRight.duration(500).delay(400)}>
-                    <Button mode="text" onPress={() => setIsSignUp(!isSignUp)}>
-                        {isSignUp ? "Already have an account? Sign In!" : "Don't have an account? Sign Up!"}
-                    </Button>
-                </Animated.View>
+                </Button>
+                <Button mode='text' textColor='white' onPress={() => {
+                    setIsSignUp(!isSignUp);
+                    setInputKey(a => a + 1)
+                }}>
+                    {isSignUp ? "Already have an account? Sign In!" : "Don't have an account? Sign Up!"}
+                </Button>
             </View>
-        </View>
+            <MotiView style={{ width: 120, height: 120, backgroundColor: "#57379a", position: "absolute", zIndex: 0, filter: "blur(10px); brightness(0.6)", left: 0, top: 0 }}
+                from={{
+                    left: 0, top: 0
+                }}
+                animate={{
+                    left: randomX,
+                    top: randomY
+                }}
+                transition={{ duration: 5000, loop: true, type: 'timing' }}
+                onDidAnimate={() => {
+                    setRandomX(Math.random() * width - 120)
+                    setRandomY(Math.random() * height - 120)
+                }}
+            />
+            <MotiView style={{ width: 120, height: 120, backgroundColor: "#57379a", position: "absolute", zIndex: 0, filter: "blur(10px); brightness(0.6)", left: 0, top: 0 }}
+                from={{
+                    left: 0, top: 0
+                }}
+                animate={{
+                    left: randomX2,
+                    top: randomY2
+                }}
+                transition={{ duration: 5000, loop: true, type: 'timing' }}
+                onDidAnimate={() => {
+                    setRandomX2(Math.random() * width - 120)
+                    setRandomY2(Math.random() * height - 120)
+                }}
+            />
+        </KeyboardAvoidingView>
     )
 }
 export default AuthScreen
