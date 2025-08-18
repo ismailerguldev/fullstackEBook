270e03f13f166b109bd28eae2a0a2ff33ab0d323
ismailerguldev <ismailergul.dev@gmail.com>
2025-08-18 04:27:38 +0300

    Favorites Page Updated

--- frontend/App/Screens/Home/FavoritesPage.tsx
+++ frontend/App/Screens/Home/FavoritesPage.tsx
@@ -1,15 +1,15 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, Dimensions } from 'react-native';
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../Components/UserContext';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
-import BookBox from '../../Components/BookBox'; 
+import BookBox from '../../Components/BookBox';
 import { UserValues } from '../../Sources/Models/models';
 
 const FavoritesPage = () => {
     const tempUser = useContext(UserContext);
     const [user, setUser] = useState<UserValues>(tempUser);
     const [key, setKey] = useState<number>(0);
-
+    const { width, height } = Dimensions.get("screen")
     useFocusEffect(
         useCallback(() => {
             setKey(a => a + 1);
@@ -18,9 +18,10 @@ const FavoritesPage = () => {
     );
 
     return (
-        <View key={key} style={{ flex: 1, alignItems: "center", padding: 25, }}>
+        <View key={key} style={{ flex: 1, padding: 25, alignItems: "center", backgroundColor: "#151515", paddingTop: 70, gap: 20 }}>
+            <Text style={{ color: "white", fontSize: 24, fontWeight: 500, alignSelf: "flex-start" }}>Favorites</Text>
             {user.favoriteBooks.length > 0 ? (
-                <ScrollView contentContainerStyle={{ flexDirection: "row", gap: 15, flexWrap: "wrap" }}>
+                <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ gap: 15, minHeight: height }}>
                     {user.favoriteBooks.map(book => (
                         <BookBox key={book.id} book={book} />
                     ))}
